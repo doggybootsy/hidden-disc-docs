8c7e2b57561364f0e4041236a1e134c8a5d5190a
1305 <1305@national.shitposting.agency>
2022-05-05 22:57:57 +0000

    Create toggleExperiment.js

---
+++ snippets/toggleExperiment.js
@@ -0,0 +1,39 @@
+function toggleExperiment(NAME, t=1) {
+    var EXPobj; var isExperimentOn;
+    window.webpackChunkdiscord_app.push([
+        [Math.random()], {}, (e) => {EXPobj = EXPobj || Object.values(e.c).filter(m => m?.exports?.default?.definition?.id?.includes(NAME))[0]}]);
+    isExperimentOn = EXPobj?.exports?.default?.getCurrentConfig()?.enabled;
+    if (isExperimentOn == false) {
+        window.webpackChunkdiscord_app.push([
+            [Math.random()], {},
+            function({c: e}) {
+                for (let c in e) {
+                    let exps = e[c]?.exports;
+                    if (exps && typeof exps.overrideBucket === "function" && exps.ExperimentStore) {
+                        for (var i = 0; i < 2; i += 1) {
+                            setTimeout(() => {
+                                exps.overrideBucket(EXPobj?.exports?.default?.definition?.id, t);
+                            }, 100);
+                        }
+                    }
+                }
+            }
+        ]);
+        console.log(`Enabled experiment ${EXPobj?.exports?.default?.definition?.label} (${EXPobj?.exports?.default?.definition?.id}) with treatment ${t}!`);
+    } else if (isExperimentOn == true) {
+        window.webpackChunkdiscord_app.push([
+            [Math.random()], {},
+            function({
+                c: e
+            }) {
+                for (let c in e) {
+                    let exps = e[c]?.exports;
+                    if (exps && typeof exps.overrideBucket === "function" && exps.ExperimentStore) {
+                        exps.overrideBucket(EXPobj?.exports?.default?.definition?.id, null);
+                    }
+                }
+            }
+        ]);
+        console.log(`Disabled experiment ${EXPobj?.exports?.default?.definition?.label} (${EXPobj?.exports?.default?.definition?.id})!`);
+    }
+}
