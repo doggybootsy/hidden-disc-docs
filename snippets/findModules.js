1a4549d91c9e0cf9752e3354214b9d5e1c483159
1305 <1305@national.shitposting.agency>
2022-05-13 16:55:37 -0500

    Update findModules.js

--- snippets/findModules.js
+++ snippets/findModules.js
@@ -8,6 +8,7 @@ function findModules(n, b) {
             m.push(...Object.values(e.c).filter(m => m?.exports && ((m?.exports?.default && Object.keys(m.exports.default).some(key => key.toLowerCase().includes(n))) || (m.exports?.default?.prototype && Object.keys(m.exports.default.prototype).some(key => key.toLowerCase().includes(n))) || Object.keys(m.exports).some(key => key.toLowerCase().includes(n)))))
         }
     ]);
+    webpackChunkdiscord_app.pop();
     if (d) {
         m.forEach(f => m.push(typeof f?.exports?.default === "undefined" ? f?.exports : f?.exports?.default));
         for (var i = 0; i < m.length; i++) {
